5179e33f1a47b6831ad4e18c1677291dea2aea3a
Vinicius <viniciusvieiradearaujo@hotmail.com>
2019-09-18 15:21:56 -0300

    english support

--- strusion.py
+++ strusion.py
@@ -1,51 +1,52 @@
 import argparse
 from IO import IO
 
-io = IO() # objeto de entrada e saída de dados
+io = IO() # object for I/O
 
-parser = argparse.ArgumentParser(description="Fusor de Legendas")
+parser = argparse.ArgumentParser(description="Subtitle Fuser")
 
-parser.add_argument("-l","--legendas",nargs=2,help="Legendas para a fusao", required=True)
-parser.add_argument("-o","--output",nargs=1,help="Nome do arquivo a ser gerado", required=True)
-parser.add_argument("-x","--x_axis",nargs=1,help="Quantidade de pixes de distancia no eixo de X", required=False)
-parser.add_argument("-y","--y_axis",nargs=1,help="Quantidade de pixes de distancia no eixo de Y", required=False)
+parser.add_argument("-S","--subtitles",nargs=2,help="Subtitles to fuse", required=False)
+parser.add_argument("-s","--subtitle",nargs=1,help="Subtitle", required=False)
+parser.add_argument("-o","--output",nargs=1,help="output file", required=True)
+parser.add_argument("-x","--x_axis",nargs=1,help="X axis pixels that stays above", required=False)
+parser.add_argument("-y","--y_axis",nargs=1,help="Y axis pixels that stays above", required=False)
 
-args = parser.parse_args()	# Atribuindo os argumentos a uma variável
+args = parser.parse_args()	
 
-if args.legendas: #verficando se os argumentos estão presentes
-	leg1 = io.load(args.legendas[0])	#carregando o arquivo
-	leg2 = io.load(args.legendas[1])	# ''
+if args.subtitles: # non empty args
+	leg1 = io.load(args.subtitles[0])	#loading file
+	leg2 = io.load(args.subtitles[1])	# ''
 	
-	x_axis = 200 	# posição padrão do eixo de x
-	y_axis = 45		# posição padrão do eixo de y
+	x_axis = 200 	# x axis for second sub
+	y_axis = 45		# y axis for second sub
 
-	if args.x_axis:				# Verifica se o parametro foi definido para adicionalo
+	if args.x_axis:				
 		x_axis = args.x_axis[0]
 	if args.y_axis:
 		y_axis = args.y_axis[0]		
 
 	position = "{\pos(" + str(x_axis) + "," + str(y_axis) + ")}" # Monta a string de posição para adionar na legenda	
 
-	control = False #	Variavel de controle
+	control = False #	control var
+	leg1.append('')									# adds a empty line
+	for i in range(len(leg2)):						# running througth every second line sub
 
-	leg1.append('')									# Adicionando uma linha vazia
-	for i in range(len(leg2)):						# Percorrendo todas as linhas da segunda legenda
-
-		if 'ï»¿1' in leg2[i]:						# Verificando a existência de um caractere especial	
-				leg2[i] = leg2[i][len(leg2[i])-1]	# Removendo o caractere especial
+		if 'ï»¿' in leg2[i]:						# special character verification	
+				leg2[i] = leg2[i][len(leg2[i])-1]	# removing it
 		
 		if control:
-			leg1.append(position+leg2[i])			# acidionando o a linha atual da legenda 2 no final da legenda 1 + posição
-			control = False
+			leg1.append(position+leg2[i])			# adding the second sub line to the end of first sub + position x and y
+			control = False							
 		else:			
-			leg1.append(leg2[i])					# adicionando sem posição
+			leg1.append(leg2[i])					# adding without position
 		
 
-		if '-->' in leg2[i]:						# Verifica se é a linha correta para inserir a posição
+		if '-->' in leg2[i]:						# cheking if the line needs position params 
 			control = True
 
-	io.outputfile(args.output[0], leg1) 			# Gravando a legenda 1 (com o acréscimo da legenda 2) em um arq srt
-
+	io.outputfile(args.output[0], leg1) 			#put into a srt file
+elif args.subtitle:
+	print('Unica sub')
 else:
 	print("Argumentos insuficientes")
 	exit()
