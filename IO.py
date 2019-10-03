8871e0e32f2aeca4505931d079d35dbe48dbd2cb
Vinicius <viniciusvieiradearaujo@hotmail.com>
2019-10-03 15:26:51 -0300

    one sub support and positions configs

--- IO.py
+++ IO.py
@@ -5,18 +5,27 @@ class IO:
 
     def load(self, path): #abre o aquivo e transforma em lista
         try:
-            objTxt = open(path) # por padrão é aberto em r
+            objTxt = open(path, encoding="utf8") # por padrão é aberto em r
+            re = list(objTxt.read().splitlines())
+        
+        except UnicodeDecodeError:
+            objTxt = open(path, encoding="ISO-8859-1")
+            re = list(objTxt.read().splitlines())
+
         except FileNotFoundError:
+
             print("O Aquivo não foi encontrado")
             exit()      #sai do script
-        re = list(objTxt.read().splitlines())
+        
+        
         objTxt.close()
         for ele in range(0, len(re)):
             re[ele] = re[ele].strip() # eliminando quarquer tipo de espaço em cada linha
         return re
 
     def outputfile(self, outputpath, lista): # joga os dados em um txt
-        o = open(outputpath, 'a+')
+        o = open(outputpath, 'a+', encoding="utf8")
+        
         for a in lista:
             o.write(f"{a}\n")
         o.close()
--- strusion.py
+++ strusion.py
@@ -5,48 +5,106 @@ io = IO() # object for I/O
 
 parser = argparse.ArgumentParser(description="Subtitle Fuser")
 
+
 parser.add_argument("-S","--subtitles",nargs=2,help="Subtitles to fuse", required=False)
 parser.add_argument("-s","--subtitle",nargs=1,help="Subtitle", required=False)
 parser.add_argument("-o","--output",nargs=1,help="output file", required=True)
 parser.add_argument("-x","--x_axis",nargs=1,help="X axis pixels that stays above", required=False)
 parser.add_argument("-y","--y_axis",nargs=1,help="Y axis pixels that stays above", required=False)
+parser.add_argument("-c","--color",nargs=1,help="subtitle color", required=False)
+
 
 args = parser.parse_args()	
 
-if args.subtitles: # non empty args
-	leg1 = io.load(args.subtitles[0])	#loading file
-	leg2 = io.load(args.subtitles[1])	# ''
-	
-	x_axis = 200 	# x axis for second sub
-	y_axis = 45		# y axis for second sub
+#Default values
+x_axis = 192 			# x axis for second sub
+y_axis = 48				# y axis for second sub
+y_axis_bottom = 268		# y axis for second sub
 
-	if args.x_axis:				
-		x_axis = args.x_axis[0]
-	if args.y_axis:
-		y_axis = args.y_axis[0]		
+sub_position_str = "{\pos(" + str(x_axis) + "," + str(y_axis) + ")}"
+sub_color = "ffffff"
 
-	position = "{\pos(" + str(x_axis) + "," + str(y_axis) + ")}" # Monta a string de posição para adionar na legenda	
-
-	control = False #	control var
-	leg1.append('')									# adds a empty line
-	for i in range(len(leg2)):						# running througth every second line sub
-
-		if 'ï»¿' in leg2[i]:						# special character verification	
-				leg2[i] = leg2[i][len(leg2[i])-1]	# removing it
+def main():
+	if args.subtitles: # non empty args for 2 subs
+		leg1 = io.load(args.subtitles[0])	#loading file
+		leg2 = io.load(args.subtitles[1])	# ''
 		
-		if control:
-			leg1.append(position+leg2[i])			# adding the second sub line to the end of first sub + position x and y
-			control = False							
-		else:			
-			leg1.append(leg2[i])					# adding without position
+		banner()
+		sub_position_str = setPosition()	 	
+		color_str = setColor()
+
+		control = False #	control var to indicates that is a new sub block
+		leg1.append('')									
+
+		for i in range(len(leg2)):						
+
+			if 'ï»¿' in leg2[i]:							
+				leg2[i] = leg2[i][len(leg2[i])-1]	
+
+			if control:													# goes into because is the firstline without -->
+				leg1.append(sub_position_str+color_str+leg2[i])  		# add the first line
+				control = False							
+
+			elif leg2[i] != '':
+				leg1.append(leg2[i])									# add the subsequent lines			
+
+			else:			
+				leg1.append("</font>")									# add the last line
+				leg1.append("")
+			
+			if '-->' in leg2[i]:										# from here, the code goes to a new sub block
+				control = True
+				
+
+
+		io.outputfile(args.output[0], leg1) 			#put into a srt file
+
+	elif args.subtitle:		#one sub 								
+		leg1 = io.load(args.subtitle[0])
+		legOut = list()
+
+		banner()
+		sub_position_str = setPosition()	 	
+		color_str = setColor()
 		
+		control = False
+		leg1.append('')									
+		for i in range(len(leg1)):
 
-		if '-->' in leg2[i]:						# cheking if the line needs position params 
-			control = True
+			if 'ï»¿' in leg1[i]:							
+				leg1[i] = leg1[i][len(leg1[i])-1]
 
-	io.outputfile(args.output[0], leg1) 			#put into a srt file
-elif args.subtitle:
-	print('Unica sub')
-else:
-	print("Argumentos insuficientes")
-	exit()
+			if control:
+				legOut.append(sub_position_str+color_str+leg1[i])
+				control = False
+
+			elif leg1[i] != '':
+				legOut.append(leg1[i])									# add the subsequent lines			
+
+			else:			
+				legOut.append("</font>")									#add the last line
+				legOut.append("")
+			
+			if '-->' in leg1[i]:										# from here, the code goes to a new sub block
+				control = True
+
+		io.outputfile(args.output[0], legOut)
+
+
+def setPosition():	
+	''' 
+	two different position exists for the y axis, that's why the extra 'if' in y,
+	'''
+	x = args.x_axis[0] if args.x_axis else x_axis
+	y =	args.y_axis[0] if args.y_axis else (y_axis_bottom if args.subtitle else y_axis)
+
+	return "{\pos(" + str(x) + "," + str(y) + ")}"
+
+def setColor():
+	return "<font color=\""+(args.color[0] if args.color else sub_color)+"\">"
+
+
+main()
+
+
+
