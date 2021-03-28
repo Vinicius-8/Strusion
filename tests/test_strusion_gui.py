87e44e8f916bc42652fbe87a23e9c1a392bd9010
Vinicius <viniciusvieiradearaujo@hotmail.com>
2021-03-28 19:17:17 -0300

    testing for gui

--- tests/test_strusion_gui.py
+++ tests/test_strusion_gui.py
@@ -11,16 +11,24 @@ sys.path.insert(0, parent_dir)
 
 from Strusion.gui import strusion_gui as gui
 
-app = QtWidgets.QApplication(sys.argv)
-MainWindow = QtWidgets.QMainWindow()
-ui = gui.Ui_MainWindow()
-ui.setupUi(MainWindow)
-# MainWindow.show()
 
 def test_variables():
-	ui.label
-	ui.label_path
-	ui.button_load
-	ui.spinBox
-	ui.button_run
-	ui.actionOpen	
+    app = QtWidgets.QApplication(sys.argv)
+ 
+    MainWindow = QtWidgets.QMainWindow()
+    ui = gui.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+
+    ui.label
+    ui.label_path
+    ui.button_load
+    ui.spinBox
+    ui.button_run
+    ui.actionOpen
+  
+
+    
+    #ui = Ui_MainWindow()
+    #ui.setupUi(MainWindow)
+    #MainWindow.show()
+    #sys.exit(app.exec_())
